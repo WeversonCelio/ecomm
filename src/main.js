fd59d16b84ab9790b7443dab8e6f3288933eb231
Weverson Celio <weverson.celio@pagonxt.com>
2023-06-01 10:40:26 -0300

    Criando imagem do ecomm

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
+// Strict mode makes it easier to write "secure" JavaScript
+'use Strict'
+
 console.log('Iniciando ecomm');
+
+// Import
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT  // port passed in docker
+
+//App
+const app = express();
+app.get('/', (req, res)=>{
+    res.send('Ecomm: Em breve!!!');
+    console.log(`'Running in port ${PORT}`);
+});
+
+app.listen(PORT);
+
+
+
+
+
+
+
+
+
+
+
+
